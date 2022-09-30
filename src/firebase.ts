2ba65a08784dc36e92214b841c2d2776681a8411
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-09-30 09:53:57 -0300

    feat: add firebase initial configuration

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: 'chira-e-commerce.firebaseapp.com',
+	projectId: 'chira-e-commerce',
+	storageBucket: 'chira-e-commerce.appspot.com',
+	messagingSenderId: '1057326651291',
+	appId: '1:1057326651291:web:2807825c5621e2d5a2b33e',
+}
+console.log(firebaseConfig)
+const app = initializeApp(firebaseConfig)
