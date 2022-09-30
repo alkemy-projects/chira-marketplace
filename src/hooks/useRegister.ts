0da244a8b644341b1adaf2e23d1ea09fcf1c6455
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-09-30 18:50:25 -0300

    feat: add sweetalert on register error

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,5 +1,6 @@
 import { collection, addDoc } from 'firebase/firestore'
 import db from '../firebase'
+import Swal from 'sweetalert2'
 
 interface Values {
 	name?: string
@@ -24,7 +25,16 @@ export const useRegister = () => {
 			console.log('Document written with ID: ', docRef.id)
 		} catch (e) {
 			console.error('Error adding document: ', e)
+			fireSweetAlert('Ha ocurrido un error inesperado', e)
 		}
+	}
+
+	const fireSweetAlert = (title: string, text: any) => {
+		Swal.fire({
+			title: title,
+			text: text,
+			confirmButtonColor: '#1E5128',
+		})
 	}
 
 	return { handleSubmit }
