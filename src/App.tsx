cc6abfc88cf384668dd9e3761fc1d094c197ec44
juanIgnacio <juan.melgarejobrabo@gmail.com>
2022-10-07 13:35:40 -0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './sections/Register/Register'
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import Login from './sections/Login/Login'
 
-export default function App() {
-	
-=======
-
-export default function App() {
->>>>>>> 215845deea05328d3dc9fc54f37012b4e115f22e
-=======
-import Login from './sections/login/Login'
 import { useSelector } from 'react-redux'
 import Loader from './components/Loader/Loader'
 
@@ -19,7 +10,7 @@ import Loader from './components/Loader/Loader'
 export default function App() {
 	const loader = useSelector((state: any) => state.loader)
 
->>>>>>> auth
+
 	return (
 		<div className='App'>
 			{loader.status && <Loader progress={loader.progress} />}
@@ -29,19 +20,11 @@ export default function App() {
 						path='/register'
 						element={<Register />}
 					/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> auth
+
 					<Route
 						path='/login'
 						element={<Login />}
 					/>
-<<<<<<< HEAD
-=======
->>>>>>> 215845deea05328d3dc9fc54f37012b4e115f22e
-=======
->>>>>>> auth
 				</Routes>
 			</BrowserRouter>
 		</div>
