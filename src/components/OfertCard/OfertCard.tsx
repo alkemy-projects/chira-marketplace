f2c02988f26daf6e5b099b0ad8e862f94c30244d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-17 13:35:46 -0300

    feat: improve offers styles

--- src/components/OfertCard/OfertCard.tsx
+++ src/components/OfertCard/OfertCard.tsx
@@ -1,24 +1,24 @@
 export const OfertCard = ({
-    ofertText,
-	image
+	ofertText,
+	image,
 }: {
-	ofertText: string,
+	ofertText: string
 	image: string
-	
-})=>{
-  
-    return(
-        <div className="ofertCardCont">
-            
-            <div className="ofertText">
-                <p>{ofertText}</p>
-                <h1>Hasta 25% off!</h1>
-                <h1>y 6 cuotas sin interes</h1>
-                <button>Ver mas</button>
-            </div>
-            <div className="ofertImg">
-                <img src={image} alt="" />
-            </div>
-        </div>
-    )
+}) => {
+	return (
+		<div className='ofertCardCont'>
+			<div className='ofertText'>
+				<p>{ofertText}</p>
+				<h3>Hasta 25% off!</h3>
+				<h3>y 6 cuotas sin interes</h3>
+				<button>Ver mas</button>
+			</div>
+			<div className='ofertImg'>
+				<img
+					src={image}
+					alt=''
+				/>
+			</div>
+		</div>
+	)
 }
