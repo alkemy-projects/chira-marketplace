376b816e30bd2cf8fe5e475b844e33b4a4e635a5
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-24 12:56:49 -0300

    fix: fix product list style

--- src/sections/ProductsList/ProductsList.tsx
+++ src/sections/ProductsList/ProductsList.tsx
@@ -26,6 +26,7 @@ export const ProductsList = () => {
 	useEffect(() => {
 		const bringProducts = async () => {
 			const data = await getResultsByQuery(search.search)
+			console.log(data.results)
 			setProducts(data.results)
 		}
 
@@ -35,56 +36,57 @@ export const ProductsList = () => {
 	return (
 		<>
 			<Header />
-			<div className='selectPrice'>
-				<select
-					name='ordenar'
-					id=''
-					style={{ height: '30px' }}
-					onChange={e => setOrder(e.target.value)}
-				>
-					<option value='low'>low price</option>
-					<option value='high'>high price</option>
-				</select>
-			</div>
-			<div className='listCont'>
-				{order == 'low' &&
-					products
-						.sort((productsa, productsb) =>
-							productsa.price > productsb.price ? 1 : -1
-						)
-						.map(product => (
-							<div
-								className='listCards'
-								key={product.id}
-							>
-								<Cards
-									title={short(product.title)}
-									image={product.thumbnail}
-									price={product.price}
-									id={product.id}
-								/>
-							</div>
-						))}
-				{order == 'high' &&
-					products
-						.sort((productsa, productsb) =>
-							productsa.price > productsb.price ? 1 : -1
-						)
-						.reverse()
-						.map(product => (
-							<div
-								className='listCards'
-								key={product.id}
-							>
-								<Cards
-									title={short(product.title)}
-									image={product.thumbnail}
-									price={product.price}
-									id={product.id}
-								/>
-							</div>
-						))}
-			</div>
+			<section className='product-list'>
+				<h1>{search.search}</h1>
+				<div className='product-list-wrapper'>
+					<div className='filters'>
+						<select
+							name='ordenar'
+							id=''
+							style={{ height: '30px' }}
+							onChange={e => setOrder(e.target.value)}
+						>
+							<option value='low'>Más baratos</option>
+							<option value='high'>Más caros</option>
+						</select>
+					</div>
+					<div className='listCont'>
+						{order == 'low' &&
+							products
+								.sort((productsa, productsb) =>
+									productsa.price > productsb.price ? 1 : -1
+								)
+								.map(product => (
+									<Cards
+										title={short(product.title)}
+										image={product.thumbnail}
+										price={product.price}
+										id={product.id}
+										key={product.id}
+									/>
+								))}
+						{order == 'high' &&
+							products
+								.sort((productsa, productsb) =>
+									productsa.price > productsb.price ? 1 : -1
+								)
+								.reverse()
+								.map(product => (
+									<div
+										className='listCards'
+										key={product.id}
+									>
+										<Cards
+											title={short(product.title)}
+											image={product.thumbnail}
+											price={product.price}
+											id={product.id}
+										/>
+									</div>
+								))}
+					</div>
+				</div>
+			</section>
 		</>
 	)
 }
